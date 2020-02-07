ae1d423d09cad584a6f2ed905c468c837f900d5c
George Thayamkery <george.thayamkery@gmail.com>
2020-02-07 17:45:00 -0600

    just some linting and stuff

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    rules: {
+        indent: ["error", 4]
+    }
+};
